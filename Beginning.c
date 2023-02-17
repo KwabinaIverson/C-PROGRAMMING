9f9970e1ae492164f37f8d4c35f15ad7631538b5
KwabinaIverson <bredrenbonish@gmail.com>
2023-02-17 20:15:19 +0000

    first commit

---
+++ Beginning.c
@@ -0,0 +1,32 @@
+/* First C Programme */
+#include <stdio.h>
+
+int main()
+{
+  /* OPERATIONS */
+  /* ADDITION, SUBTRACTION, MULTIPLICATION, DIVISION AND MODULE */
+
+  /* ADDITION */
+  int var1 = 14, var2 = 23;
+  printf("%d \n", var1 + var2);
+
+  /* SUBTRACTION */
+  printf("%d \n", var2 - var1);
+
+  /* MULTIPLICATION */
+  printf("%d \n", var1 * var2);
+
+  /* DIVISION */
+  printf("%d \n", var2 / var1);
+
+  /* MODULE */
+  printf("%d \n", var2 % var1);
+
+  /* BITWISE << */
+  printf("%d \n", var1 << 2);
+
+  /* BITWISE >> */
+  printf("%d", var2 >> 3);
+
+  return 0;
+}
